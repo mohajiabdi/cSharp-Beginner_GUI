2e1affdb4adc620b3cbf8109af4180182b58c4ef
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-05-05 15:18:45 +0300

    new

---
+++ ListBoxes and More/ListBoxes and More/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ListBoxes_and_More
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            lblMessage = new Label();
+            txtCountry = new TextBox();
+            lstCountryNames = new ListBox();
+            btnInsert = new Button();
+            btnAdd = new Button();
+            btnClear = new Button();
+            btnRemove = new Button();
+            btnFind = new Button();
+            btnCount = new Button();
+            btnSelectedIndex = new Button();
+            btnSelectedItem = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(12, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(302, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Enter Country Name: ";
+            // 
+            // lblMessage
+            // 
+            lblMessage.BackColor = SystemColors.ActiveCaption;
+            lblMessage.BorderStyle = BorderStyle.Fixed3D;
+            lblMessage.Font = new Font("Segoe UI", 18F);
+            lblMessage.ForeColor = Color.White;
+            lblMessage.Location = new Point(27, 410);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(591, 130);
+            lblMessage.TabIndex = 1;
+            // 
+            // txtCountry
+            // 
+            txtCountry.Font = new Font("Segoe UI", 18F);
+            txtCountry.Location = new Point(305, 19);
+            txtCountry.Name = "txtCountry";
+            txtCountry.Size = new Size(287, 47);
+            txtCountry.TabIndex = 2;
+            // 
+            // lstCountryNames
+            // 
+            lstCountryNames.Font = new Font("Segoe UI", 18F);
+            lstCountryNames.FormattingEnabled = true;
+            lstCountryNames.ItemHeight = 41;
+            lstCountryNames.Items.AddRange(new object[] { "Somalia", "Kenya", "Ethiopia" });
+            lstCountryNames.Location = new Point(27, 93);
+            lstCountryNames.Name = "lstCountryNames";
+            lstCountryNames.Size = new Size(243, 291);
+            lstCountryNames.TabIndex = 3;
+            lstCountryNames.SelectedIndexChanged += lstCountryNames_SelectedIndexChanged;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Font = new Font("Segoe UI", 13.8F);
+            btnInsert.Location = new Point(476, 93);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(142, 65);
+            btnInsert.TabIndex = 4;
+            btnInsert.Text = "INSERT";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Segoe UI", 13.8F);
+            btnAdd.Location = new Point(288, 93);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(142, 65);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "ADD";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI", 13.8F);
+            btnClear.Location = new Point(476, 164);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(142, 65);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "CLEAR";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Font = new Font("Segoe UI", 13.8F);
+            btnRemove.Location = new Point(288, 164);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(142, 65);
+            btnRemove.TabIndex = 7;
+            btnRemove.Text = "REMOVE";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += btnRemove_Click;
+            // 
+            // btnFind
+            // 
+            btnFind.Font = new Font("Segoe UI", 13.8F);
+            btnFind.Location = new Point(476, 235);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(142, 65);
+            btnFind.TabIndex = 8;
+            btnFind.Text = "FIND";
+            btnFind.UseVisualStyleBackColor = true;
+            btnFind.Click += btnFind_Click;
+            // 
+            // btnCount
+            // 
+            btnCount.Font = new Font("Segoe UI", 13.8F);
+            btnCount.Location = new Point(288, 235);
+            btnCount.Name = "btnCount";
+            btnCount.Size = new Size(142, 65);
+            btnCount.TabIndex = 9;
+            btnCount.Text = "COUNT";
+            btnCount.UseVisualStyleBackColor = true;
+            btnCount.Click += btnCount_Click;
+            // 
+            // btnSelectedIndex
+            // 
+            btnSelectedIndex.Font = new Font("Segoe UI", 13.8F);
+            btnSelectedIndex.Location = new Point(288, 306);
+            btnSelectedIndex.Name = "btnSelectedIndex";
+            btnSelectedIndex.Size = new Size(142, 81);
+            btnSelectedIndex.TabIndex = 10;
+            btnSelectedIndex.Text = "Selected Index";
+            btnSelectedIndex.UseVisualStyleBackColor = true;
+            btnSelectedIndex.Click += btnSelectedIndex_Click;
+            // 
+            // btnSelectedItem
+            // 
+            btnSelectedItem.Font = new Font("Segoe UI", 13.8F);
+            btnSelectedItem.Location = new Point(476, 306);
+            btnSelectedItem.Name = "btnSelectedItem";
+            btnSelectedItem.Size = new Size(142, 81);
+            btnSelectedItem.TabIndex = 11;
+            btnSelectedItem.Text = "Selected Item";
+            btnSelectedItem.UseVisualStyleBackColor = true;
+            btnSelectedItem.Click += btnSelectedItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(643, 584);
+            Controls.Add(btnSelectedItem);
+            Controls.Add(btnSelectedIndex);
+            Controls.Add(btnCount);
+            Controls.Add(btnFind);
+            Controls.Add(btnRemove);
+            Controls.Add(btnClear);
+            Controls.Add(btnAdd);
+            Controls.Add(btnInsert);
+            Controls.Add(lstCountryNames);
+            Controls.Add(txtCountry);
+            Controls.Add(lblMessage);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "ListBox";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblMessage;
+        private TextBox txtCountry;
+        private ListBox lstCountryNames;
+        private Button btnInsert;
+        private Button btnAdd;
+        private Button btnClear;
+        private Button btnRemove;
+        private Button btnFind;
+        private Button btnCount;
+        private Button btnSelectedIndex;
+        private Button btnSelectedItem;
+    }
+}
---
+++ ListBoxes and More/ListBoxes and More/Form1.cs
@@ -0,0 +1,155 @@
+namespace ListBoxes_and_More
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtCountry.Text == "")
+            {
+                lblMessage.Text = "Write a Country Name in the TextBox!...";
+                
+            }
+            else
+            {
+
+                lstCountryNames.Items.Add(txtCountry.Text);
+               
+            }
+            txtCountry.Text = "";
+            txtCountry.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstCountryNames.Items.Clear();
+            txtCountry.Text = "";
+            txtCountry.Focus();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "The Total List is: " + lstCountryNames.Items.Count.ToString();
+            txtCountry.Text = "";
+            txtCountry.Focus();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            int index = lstCountryNames.SelectedIndex = -1;
+            index = lstCountryNames.FindString(txtCountry.Text);
+
+
+            if (index != ListBox.NoMatches)
+            {
+
+                string foundItemText = lstCountryNames.Items[index].ToString();
+                lblMessage.Text = "Found Item is : " + foundItemText;
+
+
+            }
+            else
+            {
+                lblMessage.Text = "Item Not Found";
+
+            }
+            txtCountry.Text = "";
+            txtCountry.Focus();
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstCountryNames.SelectedItem != null)
+            {
+                lstCountryNames.Items.Remove(lstCountryNames.SelectedItem);
+                lblMessage.Text = "Selected Item Removed!...";
+            }
+            else
+            {
+                lblMessage.Text = "No Item Selected!...";
+            }
+            txtCountry.Text = "";
+            txtCountry.Focus();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (lstCountryNames.SelectedIndex != ListBox.NoMatches)
+            {
+                if (txtCountry.Text != "")
+                {
+                    lstCountryNames.Items.Insert(lstCountryNames.SelectedIndex, txtCountry.Text);
+                    
+                    lblMessage.Text = "New Item Inserted!...";
+                }
+                else
+                {
+                    lblMessage.Text = "Write a Country Name in the TextBox!...";
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Please Select An Item!..";
+            }
+            txtCountry.Text = "";
+            txtCountry.Focus();
+        }
+
+        private void btnSelectedItem_Click(object sender, EventArgs e)
+        {
+            int index = lstCountryNames.SelectedIndex;
+
+            if (index != ListBox.NoMatches)
+            {
+
+                string foundItemText = lstCountryNames.Items[index].ToString();
+                lblMessage.Text = "Selected Item is : " + foundItemText;
+                txtCountry.Text = "";
+                txtCountry.Focus();
+
+            }
+            else
+            {
+                lblMessage.Text = "No Item Selected!..";
+                txtCountry.Text = "";
+                txtCountry.Focus();
+            }
+        }
+
+        private void btnSelectedIndex_Click(object sender, EventArgs e)
+        {
+            int index = lstCountryNames.SelectedIndex;
+
+            if (index != ListBox.NoMatches)
+            {
+
+
+                lblMessage.Text = "Selected Index is: " + index;
+                txtCountry.Text = "";
+                txtCountry.Focus();
+
+            }
+            else
+            {
+                lblMessage.Text = "No Item Selected!...";
+                txtCountry.Text = "";
+                txtCountry.Focus();
+            }
+        }
+
+        private void lstCountryNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstCountryNames.SelectedIndex = -1;
+        }
+    }
+}
